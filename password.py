0c9c38076dbd8ee64f21cb66149df719b9076a58
Evance23 <evancebarracks01@gmail.com>
2021-04-11 18:06:43 +0300

    Add password and userna,e creation features

--- password.py
+++ password.py
@@ -1,1 +1,21 @@
 import random
+from user import user
+
+
+
+def main ():
+    
+
+    while True:
+        print("Good to see You!!!")
+        print('\n')
+        print("To create new user use 'nu': to log in use 'lg" or 'ex' to exit")
+        short_code = input().lower()
+        print('\n')
+
+        if short_code == "nu" :
+            print("create username")
+            created_user_name = input()
+
+            print ('create password')
+            created_user_password
